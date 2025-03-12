6e0c554af28e85c6f38ff06318eb6f1f40773057
TijsZ <tijs@vreijling.nl>
2025-03-12 13:25:17 +0100

    Update Reserveren.php

--- html and css possibly javascript or php/Reserveren.php
+++ html and css possibly javascript or php/Reserveren.php
@@ -1,9 +1,22 @@
-<?php include"header.php"?>
+<?php include "header.php" ?>
 <main>
-        <header2>
-            
-        </header2>
-    </main>
-    <footer>
-    </footer>
+    <title>Reserveren</title>
+    <article>
+        Voornaam
+
+        Achternaam
+
+        Telefoon nummer
+
+        Tijd van Reservatie
+
+        Aatal Personen
+        
+    </article>
+    <knop>
+        Zend
+    </knop>
+</main>
+<footer>
+</footer>
 </body>
