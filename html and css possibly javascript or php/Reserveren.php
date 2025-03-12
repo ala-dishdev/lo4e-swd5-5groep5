b9f298f3a4d9583d8b5512b280ff2f05f0d6a583
TijsZ <tijs@vreijling.nl>
2025-03-12 13:27:38 +0100

    Update Reserveren.php

--- html and css possibly javascript or php/Reserveren.php
+++ html and css possibly javascript or php/Reserveren.php
@@ -3,15 +3,15 @@
     <title>Reserveren</title>
     <article>
         Voornaam
-
+        <input type="text">
         Achternaam
-
+        <input type="text">
         Telefoon nummer
-
+        <input type="text">
         Tijd van Reservatie
-
+        <input type="date">
         Aatal Personen
-        
+        <input type="text">
     </article>
     <knop>
         Zend
