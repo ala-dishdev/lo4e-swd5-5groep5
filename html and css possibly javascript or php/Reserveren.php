515842315cd8cef5a82bbb86c7df56b0b6d00aa5
TijsZ <tijs@vreijling.nl>
2025-03-12 14:32:07 +0100

    aded labels in reserveren

--- html and css possibly javascript or php/Reserveren.php
+++ html and css possibly javascript or php/Reserveren.php
@@ -1,17 +1,17 @@
 <?php include "header.php" ?>
-<main>
-    <h1>Reserveren</h1>
+<main class="Jua">
+    <h1 class="Jua">Reserveren</h1>
     <article>
-        Voornaam
-        <input type="text">
-        Achternaam
-        <input type="text">
-        Telefoon nummer
-        <input type="number">
-        Tijd van Reservatie
-        <input type="time">
-        Aatal Personen
-        <input type="number">
+        <label for="Vnaam"> Voornaam: </label>
+        <input type="text" name="Vnaam">
+        <label for="Anaam"> Achternaam: </label>
+        <input type="text" name="Anaam">
+        <label for="Tele"> Telefoon nummer: </label>
+        <input type="number" name="Tele">
+        <label for="Tijd"> Tijd van Reservatie: </label>
+        <input type="time" name="Tijd">
+        <label for="Aperson"> Aatal Personen: </label>
+        <input type="number" name="Aperson">
     </article>
     <knop>
         <input type="button" onclick="alert('Verzonden')" value="Zend">
