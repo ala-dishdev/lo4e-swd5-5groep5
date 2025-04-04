5b3c91e40a351ae18c451ab1c9aceb6b26b93600
TijsZ <tijs@vreijling.nl>
2025-04-04 13:10:10 +0200

    finiced menu
    het was nog niet af gemaakt!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- html and css possibly javascript or php/eten.php
+++ html and css possibly javascript or php/eten.php
@@ -10,49 +10,100 @@
             <div class="columns-container">
 
                 <div class="column-left">
-                <h2>## Voorgerechten</p>
-                <p class="gerecht">** Miso Soep ** 4,50</p>
-                <p class="uitleg">Traditionele japanse soep met tofu zeewier en lente-ui</p>
-                <p class="gerecht">** Edamame ** 5,00</p>
-                <p class="uitleg">Gestoomde sojabonen met zeezout</p>
-                <p class="gerecht">** Wakame Salade ** 6,50</p>
-                <p class="uitleg">Zeewier salade met sesam dressing</p>
-                <p class="gerecht">## Nigiri Sushi (2 stuks)</p>
-                <p class="uitleg">** Sake(zalm) ** 4,50</p>
-                <p class="uitleg">** Maguro(tonijn) ** 5,00</p>
-                <p class="uitleg">** Ebi(garnaal) ** 4,50</p>
-                <p class="uitleg">** Unagi(paling) ** 5,50</p>
-                <p class="uitleg">** Tamago(ei) ** 4,00</p>
-                <p class="gerecht">** California Roll (6 stuks) ** 8,50</p>
-                <p class="uitleg">Krabstick, avocado, komkommer en masago</p>
-                <p class="gerecht">** Spicy Tuna Roll ** 9,00 (6 stuks) ** 8,50</p>
-                <p class="uitleg">Tonijn, spicy mayonaise en lente-ui</p>
+                    <h2>## Voorgerechten</p>
+                        <p class="gerecht">** Miso Soep ** 4,50</p>
+                        <p class="uitleg">Traditionele japanse soep met tofu zeewier en lente-ui</p>
+
+                        <p class="gerecht">** Edamame ** 5,00</p>
+                        <p class="uitleg">Gestoomde sojabonen met zeezout</p>
+
+                        <p class="gerecht">** Wakame Salade ** 6,50</p>
+                        <p class="uitleg">Zeewier salade met sesam dressing</p>
+
+                        <p class="gerecht">## Nigiri Sushi (2 stuks)</p>
+                        <p class="uitleg">** Sake(zalm) ** 4,50</p>
+                        <p class="uitleg">** Maguro(tonijn) ** 5,00</p>
+                        <p class="uitleg">** Ebi(garnaal) ** 4,50</p>
+                        <p class="uitleg">** Unagi(paling) ** 5,50</p>
+                        <p class="uitleg">** Tamago(ei) ** 4,00</p>
+
+                        <p class="gerecht">** California Roll (6 stuks) ** 8,50</p>
+                        <p class="uitleg">Krabstick, avocado, komkommer en masago</p>
+
+                        <p class="gerecht">** Spicy Tuna Roll ** 9,00 (6 stuks) ** 8,50</p>
+                        <p class="uitleg">Tonijn, spicy mayonaise en lente-ui</p>
+            
+                    <p class="gerecht"> ** Salmon Avocado Roll ** 9,00</p>
+                    <p class="uitleg"> Verse zalm en avocado </p>
+
+                    <p class="gerecht">**** Cucumber Roll ** 6,50</p>
+                    <p class="uitleg">komkommer en sesam(vegan)</p>
+
+                    <h2>**## special rolls (8 stuks)</h2>.
+                    <p class="gerecht">**** Dragon Roll ** 15,50 </p>
+                    <p class="uitleg">Tempura garnaal, avocado, komkommer, unagi
+                        en unagi saus</p>
+
+                    <p class="gerecht">**** Rainbow Roll ** 16,50</p>
+                    <p class="uitleg">California roll bedekt met diverse soorten vis</p>
+
+                    <p class="gerecht">**** Grispy Chicken Roll ** 16,50</p>
+                    K<p class="uitleg">rokante kip, avocado, komkommer, teriyaki
+                        saus</p>
+
+                    <p class="gerecht">**** Tempura Roll ** 15,50</p>
+                    <p class="uitleg">Gefrituurde garnaal, avocado, komkommer,
+                        spicy mayonaise</p>
                 </div>
+
                 <div class="column-right">
-                ** Salmon Avocado Roll ** 9,00
-Verse zalm en avocado
 
-** Cucumber Roll ** 6,50
-komkommer en sesam(vegan)
+                    <p class="gerecht"> ## Sashimi (5 stuks) </p>
+                    <p class="uitleg"> **Sake (Zalm)** €9,50 </p>
+                    <p class="uitleg"> **Maguro (Tonijn)** €10,50 </p>
+                    <p class="uitleg"> **Hamachi (Yellowtail)** €11,50 </p>
+                    <h2>## Combina e Menu's</h2>
 
-## special rolls (8 stuks)
-** Dragon Roll ** 15,50
-Tempura garnaal, avocado, komkommer, unagi
-en unagi saus
+                    <p class="gerecht">**Sushi Set A** €22,50 </p>
+                    <p class="uitleg">4 nigiri, 6 maki, miso soep</p>
 
-** Rainbow Roll ** 16,50
-California roll bedekt met diverse soorten vis
+                    <p class="gerecht">**Sushi Set B** €32,50 </p>
+                    <p class="uitleg">6 nigiri, 8 special roll, miso soep, edamame </p>
 
-** Grispy Chicken Roll ** 16,50
-Krokante kip, avocado, komkommer, teriyaki
-saus
+                    <p class="gerecht">**Sashimi Set** €25,50</p>
+                    <p class="uitleg">15 stuks diverse sashimi met rijst </p>
 
-** Tempura Roll ** 15,50
-Gefrituurde garnaal, avocado, komkommer,
-spicy mayonaise
+                    <p class="gerecht">**Vegetarisch Set** €20,50</p>
+                    K<p class="uitleg">4 vegetarische nigiri, 6 cucumber roll, 6 avocado roll</p>
+                    ## Bijgerechten
+                    <p class="gerecht">**** Tempura Roll ** 15,50</p>
+                    <p class="uitleg">Gefrituurde garnaal, avocado, komkommer,
+                        spicy mayonaise</p>
+          
+                    <h2>## Bijgerechten</h2>
+                    <p class="gerecht"> **Wi e Rijst** €2,50 </p>
+
+                    <p class="gerecht">**Wi e Rijst** €2,50 </p>
+
+                    <p class="gerecht">**Gember** €1,50 </p>
+
+                    <p class="gerecht">**Extra Wasabi** €1,00</p>
+
+                    <p class="gerecht">**Spicy Mayonaise** €1,00</p>
+                    <h2>## Dranken</h2>
+                    <p class="gerecht">**Groene Thee** €2,50</p>
+
+                    <p class="gerecht">**Japans Bier** €4,50</p>
+
+                    <p class="gerecht">**Sake (warm/koud)** €6,50</p>
+
+                    <p class="gerecht">**Frisdrank** €2,50</p>
+
+                    <p class="gerecht">**Mineraalwater** €2,50p </p>
+                        *Allergieën of dieetwensen? Vraag onze medewerkers naar de mogelijkheden.*
+                        *Alle prijzen zijn inclusief BTW.*
                 </div>
-
-        </div>
     </main>
-   
+    <img class="space" src="fotos/sushiphoto4.png" alt="Assorted sushi">
+    <img class="space" src="fotos/sushiphoto4.png" alt="Assorted sushi">
 </body>
